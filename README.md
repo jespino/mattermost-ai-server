71b84106c1f82d115d674c01b300f5344c1611a6
Jesús Espino <jespinog@gmail.com>
2023-05-03 11:53:39 +0200

    Some improvements on gpt-neoxt_chat_base_20b model

---
+++ README.md
@@ -0,0 +1,13 @@
+# Mattermost AI Rerver
+
+## Installation
+
+```bash
+apt-get install nvidia-cuda-toolkit mpi-default-dev
+```
+
+```bash
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+```
--- models/gpt_neoxt_chat_base_20b.py
+++ models/gpt_neoxt_chat_base_20b.py
@@ -2,23 +2,35 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 class GptNeoxChatBase20B:
-    def __init__(self, device = 'cpu'):
-        self.device = device
+    def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained("togethercomputer/GPT-NeoXT-Chat-Base-20B")
-        self.model = AutoModelForCausalLM.from_pretrained("togethercomputer/GPT-NeoXT-Chat-Base-20B", torch_dtype=torch.bfloat16).to(self.device)
+        self.model = AutoModelForCausalLM.from_pretrained("togethercomputer/GPT-NeoXT-Chat-Base-20B", device_map="auto", load_in_8bit=True)
+        self.conversation = ""
+
+    def start_conversation(self):
+        self.conversation = ""
 
     def query(self, text):
-        original_query = "<human>: {}\n<bot>:".format(text)
+        if text == "debug":
+            print(self.conversation)
+            return ""
+
+        original_query = self.conversation+"\n<human>: {}\n<bot>: ".format(text)
         current_query = original_query
+        output_str = ""
         while "<human>:" not in current_query[len(original_query):]:
             # TODO: Remove the need of tokenizing/detokenizing here (almost sure is not needed and we can speedup it a bit)
-            inputs = tokenizer(current_query, return_tensors='pt').to(self.device)
-            outputs = model.generate(**inputs, max_new_tokens=10, do_sample=True, temperature=0.8)
-            output_str = tokenizer.decode(outputs[0])
+            inputs = self.tokenizer(current_query, return_tensors='pt').to(self.model.device)
+            outputs = self.model.generate(**inputs, max_new_tokens=10, do_sample=True, temperature=0.8)
+            output_str = self.tokenizer.decode(outputs[0])
             current_query = output_str
-        head, _, _ = current_query.partition("<human>:")
+        head = current_query[len(original_query):].split("<human>:")[0]
+        self.conversation = original_query+head
         return head
 
 if __name__ == '__main__':
     gpt = GptNeoxChatBase20B()
-    gpt.query("Hello!")
+    query = input("> ")
+    while query != "quit":
+        print("bot> " + gpt.query(query))
+        query = input("> ")
