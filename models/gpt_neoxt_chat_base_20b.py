e4be6578da6e44651db7fdbfbb24fa2d779dbb03
Jesús Espino <jespinog@gmail.com>
2023-05-17 18:33:30 +0200

    Trying to make gpt_neoxt_chat a stream based bot

--- models/gpt_neoxt_chat_base_20b.py
+++ models/gpt_neoxt_chat_base_20b.py
@@ -18,15 +18,16 @@ class GptNeoxChatBase20B:
         original_query = self.conversation+"\n<human>: {}\n<bot>: ".format(text)
         current_query = original_query
         output_str = ""
-        while "<human>:" not in current_query[len(original_query):]:
+        while True:
             # TODO: Remove the need of tokenizing/detokenizing here (almost sure is not needed and we can speedup it a bit)
             inputs = self.tokenizer(current_query, return_tensors='pt').to(self.model.device)
-            outputs = self.model.generate(**inputs, max_new_tokens=10, do_sample=True, temperature=0.8)
+            outputs = self.model.generate(**inputs, max_new_tokens=1, do_sample=True, temperature=0.8)
             output_str = self.tokenizer.decode(outputs[0])
+            new_token = oputput_str[len(current_query):]
+            if new_token == "<human>:":
+                break
+            yield new_token
             current_query = output_str
-        head = current_query[len(original_query):].split("<human>:")[0]
-        self.conversation = original_query+head
-        return head
 
 if __name__ == '__main__':
     gpt = GptNeoxChatBase20B()
