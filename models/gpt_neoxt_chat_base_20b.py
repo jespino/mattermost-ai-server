03572d211b19e583aebd4227e43cbe8123587332
Jesús Espino <jespinog@gmail.com>
2023-05-17 19:30:07 +0200

    Converting gpt_neoxt_chat_base_20b into stream based api

--- models/gpt_neoxt_chat_base_20b.py
+++ models/gpt_neoxt_chat_base_20b.py
@@ -1,37 +1,37 @@
 import torch
+import sys
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 class GptNeoxChatBase20B:
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained("togethercomputer/GPT-NeoXT-Chat-Base-20B")
         self.model = AutoModelForCausalLM.from_pretrained("togethercomputer/GPT-NeoXT-Chat-Base-20B", device_map="auto", load_in_8bit=True)
-        self.conversation = ""
 
-    def start_conversation(self):
-        self.conversation = ""
-
-    def query(self, text):
-        if text == "debug":
-            print(self.conversation)
-            return ""
-
-        original_query = self.conversation+"\n<human>: {}\n<bot>: ".format(text)
+    def query(self, bot_description, text):
+        original_query = "<human>: {}\n<bot>: ok\n<human>: {}\n<bot>: ".format(bot_description, text)
         current_query = original_query
         output_str = ""
+        new_tokens = ""
         while True:
-            # TODO: Remove the need of tokenizing/detokenizing here (almost sure is not needed and we can speedup it a bit)
+            if new_tokens != "":
+                yield new_tokens
             inputs = self.tokenizer(current_query, return_tensors='pt').to(self.model.device)
-            outputs = self.model.generate(**inputs, max_new_tokens=1, do_sample=True, temperature=0.8)
+            outputs = self.model.generate(**inputs, max_new_tokens=5, do_sample=True, temperature=0.8)
             output_str = self.tokenizer.decode(outputs[0])
-            new_token = oputput_str[len(current_query):]
-            if new_token == "<human>:":
-                break
-            yield new_token
+            new_tokens = output_str[len(current_query):]
             current_query = output_str
+            if "<human>:" in current_query[len(original_query):]:
+                yield new_tokens[:new_tokens.find("<human>:")]
+                break
 
 if __name__ == '__main__':
     gpt = GptNeoxChatBase20B()
     query = input("> ")
     while query != "quit":
-        print("bot> " + gpt.query(query))
+        print("bot> ", end="")
+        for token in gpt.query("You are a helpful assistant.", query):
+            print(token, end="")
+            sys.stdout.flush()
+        print("")
+
         query = input("> ")
